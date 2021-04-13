dc9da66ae1f189b7a104339dd5d7f73ce720800c
Oliwia <62391424+olimari@users.noreply.github.com>
2021-04-13 21:12:49 +0200

    13.04.2021
    postać chodzi/skacze, wyłączenie oraz restart gry przy przegranej

---
+++ pUŁapka/pUŁapka/Form1.Designer.cs
@@ -0,0 +1,138 @@
+﻿
+namespace pUŁapka
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.player = new System.Windows.Forms.PictureBox();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.pictureBox2 = new System.Windows.Forms.PictureBox();
+            this.pictureBox3 = new System.Windows.Forms.PictureBox();
+            this.GameTimer = new System.Windows.Forms.Timer(this.components);
+            this.txtScore = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.player)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // player
+            // 
+            this.player.Image = global::pUŁapka.Properties.Resources.ruchwprawo;
+            this.player.Location = new System.Drawing.Point(3, 292);
+            this.player.Name = "player";
+            this.player.Size = new System.Drawing.Size(100, 100);
+            this.player.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.player.TabIndex = 0;
+            this.player.TabStop = false;
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = global::pUŁapka.Properties.Resources.platforma4;
+            this.pictureBox1.Location = new System.Drawing.Point(3, 388);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(357, 65);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox1.TabIndex = 1;
+            this.pictureBox1.TabStop = false;
+            this.pictureBox1.Tag = "platform";
+            // 
+            // pictureBox2
+            // 
+            this.pictureBox2.Image = global::pUŁapka.Properties.Resources.platforma4;
+            this.pictureBox2.Location = new System.Drawing.Point(473, 233);
+            this.pictureBox2.Name = "pictureBox2";
+            this.pictureBox2.Size = new System.Drawing.Size(189, 65);
+            this.pictureBox2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox2.TabIndex = 1;
+            this.pictureBox2.TabStop = false;
+            this.pictureBox2.Tag = "platform";
+            // 
+            // pictureBox3
+            // 
+            this.pictureBox3.Image = global::pUŁapka.Properties.Resources.platforma4;
+            this.pictureBox3.Location = new System.Drawing.Point(724, 109);
+            this.pictureBox3.Name = "pictureBox3";
+            this.pictureBox3.Size = new System.Drawing.Size(189, 65);
+            this.pictureBox3.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox3.TabIndex = 1;
+            this.pictureBox3.TabStop = false;
+            this.pictureBox3.Tag = "platform";
+            // 
+            // GameTimer
+            // 
+            this.GameTimer.Enabled = true;
+            this.GameTimer.Interval = 20;
+            this.GameTimer.Tick += new System.EventHandler(this.MainTimerEvent);
+            // 
+            // txtScore
+            // 
+            this.txtScore.AutoSize = true;
+            this.txtScore.Font = new System.Drawing.Font("Gill Sans Ultra Bold", 16.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.txtScore.Location = new System.Drawing.Point(0, 0);
+            this.txtScore.Name = "txtScore";
+            this.txtScore.Size = new System.Drawing.Size(173, 42);
+            this.txtScore.TabIndex = 2;
+            this.txtScore.Text = "Wynik: 0";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1482, 453);
+            this.Controls.Add(this.txtScore);
+            this.Controls.Add(this.pictureBox3);
+            this.Controls.Add(this.pictureBox2);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.player);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.FormClosed += new System.Windows.Forms.FormClosedEventHandler(this.CloseGame);
+            this.KeyDown += new System.Windows.Forms.KeyEventHandler(this.KeyIsDown);
+            this.KeyUp += new System.Windows.Forms.KeyEventHandler(this.KeyIsUp);
+            ((System.ComponentModel.ISupportInitialize)(this.player)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox player;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.PictureBox pictureBox2;
+        private System.Windows.Forms.PictureBox pictureBox3;
+        private System.Windows.Forms.Timer GameTimer;
+        private System.Windows.Forms.Label txtScore;
+    }
+}
+
---
+++ pUŁapka/pUŁapka/Form1.cs
@@ -0,0 +1,126 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace pUŁapka
+{
+    public partial class Form1 : Form
+    {
+        bool leftSide, rightSide, jump;
+        int speedPlayer = 10;
+        int speedJump = 10;
+        int force = 8;
+        int score = 0;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void KeyIsDown(object sender, KeyEventArgs e)
+        {
+            //ruch na boki
+            if (e.KeyCode == Keys.Left)
+            {
+                leftSide = true;
+            }
+            if (e.KeyCode == Keys.Right)
+            {
+                rightSide = true;
+            }
+            if (e.KeyCode == Keys.Space && jump == false)
+            {
+                jump = true;
+            }
+        }
+
+        private void MainTimerEvent(object sender, EventArgs e)
+        {
+            txtScore.Text = "Wynik: " + score;
+            player.Top += speedJump;
+
+            if (leftSide == true && player.Left > 60)
+            {
+                player.Left -= speedPlayer;
+            }
+            if (rightSide == true && player.Left + (player.Width + 60) < this.ClientSize.Width)
+            {
+                player.Left += speedPlayer;
+            }
+
+
+            if (jump == true)
+            {
+                speedJump = -12;
+                force -= 1;
+               /* player.Top -= force;
+                force -= 1;*/
+            }
+            else
+            {
+                speedJump = 12;
+            }
+
+            if (jump == true && force < 0)
+            {
+                jump = false;
+            }
+            foreach (Control x in this.Controls)
+            {
+                if (x is PictureBox && (string)x.Tag == "platform")
+                {
+                    if (player.Bounds.IntersectsWith(x.Bounds) && jump == false)
+                    {
+                        force = 8;
+                        player.Top = x.Top - player.Height;
+                        speedJump = 0;
+                    }
+                    x.BringToFront();
+                }
+            }
+
+            if (player.Top + player.Height > this.ClientSize.Height)
+            {
+                GameTimer.Stop();
+                MessageBox.Show("Przegrałeś!" + Environment.NewLine + "Kliknij OK, aby zagrać ponownie!");
+                RestartGame();
+            }
+
+
+        }
+
+        private void KeyIsUp(object sender, KeyEventArgs e)
+        {
+            if (e.KeyCode == Keys.Left)
+            {
+                leftSide = false;
+            }
+            if (e.KeyCode == Keys.Right)
+            {
+                rightSide = false;
+            }
+            if (jump == true)
+            {
+                jump = false;
+            }
+        }
+        private void RestartGame()
+        {
+            Form1 window = new Form1();
+            window.Show();
+            this.Hide();
+
+        }
+
+        private void CloseGame(object sender, FormClosedEventArgs e)
+        {
+            Application.Exit();
+        }
+   }
+}
